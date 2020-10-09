ff320945b2983ef79c9bc3033f0adc767f6d12aa
Farman <moabbasi@teksystems.com>
2020-10-10 00:04:03 +0530

    To only deploy the server

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,9 @@ const router = require('express').Router();
 let User = require('../models/user.model');
 
 router.route('/').get((req, res) => {
-  res.send("users")
-  // User.find()
-  //   .then(users => res.json(users))
-  //   .catch(err => res.status(400).json('Error: ' + err));
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post((req, res) => {
